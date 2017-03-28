5f1e0c4902c7e342be677d027ce78dbaebb9670c
Nick G <nickwgonzales@gmail.com>
2017-03-27 19:50:13 -0500

    Cleaned up server to serve static webpage

--- server.js
+++ server.js
@@ -3,26 +3,8 @@ var mime = require("mime");
 var app = express();
 var fs = require("fs");
 
-function getStatic(path){
-	return function(req,res){
-		var type = mime.lookup(path);
-		var body = fs.createReadStream(path);
-		res.writeHead(200,{"content-type":type});
-		if(body){
-			body.pipe(res);
-		}
-	}
-}
+/* Getting all static files from this directory */
 
-app.get("/",getStatic("./index.html"));
-app.get("/main.css",getStatic("./main.css"));
-app.get("/normalize.css",getStatic("./normalize.css"));
-app.get("/res/twitter.gif",getStatic("./res/twitter.gif"));
-app.get("/res/codepen.png",getStatic("./res/codepen.png"));
-app.get("/res/github.png",getStatic("./res/github.png"));
-app.get("/res/github.png",getStatic("./res/github.png"));
-app.get("/res/ee.png",getStatic("./res/ee.png"));
-app.get("/res/hearthbot.png",getStatic("./res/hearthbot.png"));
-app.get("/res/icop.png",getStatic("./res/icop.png"));
+app.use(express.static("res"));
 
 app.listen(8000);
