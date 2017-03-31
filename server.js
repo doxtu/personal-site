f90f208a292eaa336e23c42bd3654927e6be0620
Nick G <nickwgonzales@gmail.com>
2017-03-30 21:14:05 -0500

    Massive overhaul of style, addition of icop
    added:
    -icop
    -links
    -footer
    -new-style
    -rem-based typography

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 
 /* Getting all static files from this directory */
 app.use(express.static("res"));
+app.use(express.static("res/static"));
+app.use("/icop",express.static("res/static/icop"));
 
 /*set port to 80 when pushing to master*/
 app.listen(8000);
