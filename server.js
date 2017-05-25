97a44e2de964936764bc1468055121d4c50918bd
doxtu <nickwgonzales@gmail.com>
2017-05-25 17:39:38 -0500

    updated folder structure

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var express = require("express");
 var app = express();
 
 /* Getting all static files from this directory */
-app.use(express.static("res"));
-app.use(express.static("res/static"));
+app.use(express.static("public"));
 app.use("/icop",express.static("res/static/icop"));
 
 /*set port to 80 when pushing to master*/
