6f1de8b29548052ce17f3fdd1b1a6988fbbbaf6b
doxtu <nickwgonzales@gmail.com>
2017-05-25 19:27:12 -0500

    initiate database integration and test

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 var express = require("express");
 var app = express();
+var mysql = require('mysql');
 
 /* Getting all static files from this directory */
 app.use(express.static("public"));
 app.use("/icop",express.static("res/static/icop"));
+
+app.get("/mysql",function dbs(req,res){
+	let connection = mysql.createConnection({
+		host: "gonzaleslabs.com",
+		user: "root",
+		password: "Testy321!",
+		database: "test"
+	});
+	
+	connection.connect();
+	
+	connection.query("SELECT * FROM kimmy",function(err,rows,fields){
+		if(err) throw err;
+		
+		res.send(rows[0].id,rows[0].name,rows[0].song);
+	});
+	
+});
 
 /*set port to 80 when pushing to master*/
 app.listen(8000);
