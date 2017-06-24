7601e21488de7951c6a38da444dcda13b0576795
doxtu <nickwgonzales@gmail.com>
2017-06-24 18:52:32 +0000

    email form verification

---
+++ public/main.js
@@ -0,0 +1,36 @@
+const mainform = document.querySelector("form");
+const emailnotify = document.querySelector(".email-form p");
+const form = document.querySelector("form");
+
+mainform.addEventListener("submit",e=>{
+	e.preventDefault();
+	let name = form[0].value || "";
+	let email = form[1].value || "";
+	let message = form[2].value || "";
+	if(message === ""){
+		emailnotify.innerText = "Please enter a message.";
+		return;
+	}
+	let params = "person="+name+"&email="+email+"&message="+message;
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState=4){
+			if(xhr.status==200){
+				emailnotify.innerText = "Message sent!";
+			}else if(xhr.status==500){
+				emailnotify.innerText = "Server error!";
+			}
+		}
+	}
+	xhr.open("POST","/message",true);
+	xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+	xhr.send(params);
+	clearForm();
+});
+
+function clearForm(){
+	Array.prototype.forEach.call(form,elt=>{
+		if(elt.type === "submit") return;
+		elt.value = "";
+	});
+}
